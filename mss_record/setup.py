a940334f3cdd450eea58007eba5da5bdeb711415
Stefan Mertl <stefan@mertl-research.at>
2018-09-25 14:09:01 +0200

    Added the setup script.

---
+++ mss_record/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+# Get the current mss_record version, author and description.
+for line in open('lib/mss_record/__init__.py').readlines():
+    if (line.startswith('__version__')
+            or line.startswith('__author__')
+            or line.startswith('__authorEmail__')
+            or line.startswith('__description__')
+            or line.startswith('__license__')
+            or line.startswith('__keywords__')
+            or line.startswith('__website__')):
+        exec(line.strip())
+
+# Define the scripts to be processed.
+scripts = ['scripts/mss_record',]
+
+
+setuptools.setup(name = 'mss_record',
+                 version           = __version__,
+                 description       = __description__,
+                 author            = __author__,
+                 author_email      = __authorEmail__,
+                 url               = __website__,
+                 license           = __license__,
+                 keywords          = __keywords__,
+                 platforms         = 'any',
+                 scripts           = scripts,
+                 packages          = setuptools.find_packages(),
+                 install_requires  = ['Adafruit_GPIO>=0.6.5'])
+
