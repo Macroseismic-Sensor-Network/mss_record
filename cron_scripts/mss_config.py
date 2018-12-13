c0ca350e051cbc267bce512183e4aad53fde8a87
Stefan Mertl <info@mertl-research.at>
2018-12-13 18:55:57 +0100

    Check for changes in the new configuration.

--- cron_scripts/mss_config.py
+++ cron_scripts/mss_config.py
@@ -25,8 +25,8 @@ try:
     verify_result = subprocess.check_output(['gpg', '--verify', local_dali_config_sig],
                                           stderr = subprocess.STDOUT)
     verify_result = verify_result.decode("utf-8")
-    print("verify_result:")
-    print(verify_result)
+    #print("verify_result:")
+    #print(verify_result)
     # Although gpg --verify returned a success, check the output string for a
     # good signature and the expected key.
     if((verify_result.find('gpg: Good signature') != -1) and (verify_result.find('mss@mertl-research.at') != -1)):
@@ -35,19 +35,23 @@ try:
         # Update the dali config and restart the service.
         config = configparser.ConfigParser()
         config.read(dali_ini_filename)
-        config['dali']['host'] = str(new_config['dali_host'])
-        config['dali']['port'] = str(int(new_config['dali_port']))
+        config_changed = False
+        if config['dali']['host'] != str(new_config['dali_host']):
+            config['dali']['host'] = str(new_config['dali_host'])
+            config_changed = True
+        if config['dali']['port'] != new_config['dali_port']:
+            config['dali']['port'] = str(int(new_config['dali_port']))
+            config_changed = True
 
-        os.rename(dali_ini_filename, dali_ini_filename + '.old')
-        with open(dali_ini_filename, 'w') as fp:
-            config.write(fp)
+        if config_changed:
+            os.rename(dali_ini_filename, dali_ini_filename + '.old')
+            with open(dali_ini_filename, 'w') as fp:
+                config.write(fp)
 
-except Exception as e:
-    print('Verification failed.')
-    print(e)
+            # TODO: Restart the dali service.
+
 finally:
     os.remove(local_dali_config)
     os.remove(local_dali_config_sig)
-
 
 os.environ['LANGUAGE'] = orig_lang
