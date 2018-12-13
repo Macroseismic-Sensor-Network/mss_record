4c06492ef3bc3f9450c2d8a4dbaf6f97ea03f04e
Stefan Mertl <info@mertl-research.at>
2018-12-13 18:49:27 +0100

    Added the cron script to check for new dali configuration.

---
+++ cron_scripts/mss_config.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+
+import configparser
+import json
+import os
+import subprocess
+import urllib.request
+
+dali_config_url = "http://www.macroseismicsensor.at/msn_config/dali_config.json"
+dali_config_sig_url = dali_config_url + '.sig'
+local_dali_config = 'dali_config.json'
+local_dali_config_sig = local_dali_config + '.sig'
+dali_ini_filename = 'dali.ini'
+
+
+# Get the dali config file.
+urllib.request.urlretrieve(dali_config_url, local_dali_config)
+urllib.request.urlretrieve(dali_config_sig_url, local_dali_config_sig)
+
+orig_lang = os.environ['LANGUAGE']
+os.environ['LANGUAGE'] = 'en_US:en'
+
+# Verify the signature.
+try:
+    verify_result = subprocess.check_output(['gpg', '--verify', local_dali_config_sig],
+                                          stderr = subprocess.STDOUT)
+    verify_result = verify_result.decode("utf-8")
+    print("verify_result:")
+    print(verify_result)
+    # Although gpg --verify returned a success, check the output string for a
+    # good signature and the expected key.
+    if((verify_result.find('gpg: Good signature') != -1) and (verify_result.find('mss@mertl-research.at') != -1)):
+        with open(local_dali_config) as fp:
+            new_config = json.load(fp)
+        # Update the dali config and restart the service.
+        config = configparser.ConfigParser()
+        config.read(dali_ini_filename)
+        config['dali']['host'] = str(new_config['dali_host'])
+        config['dali']['port'] = str(int(new_config['dali_port']))
+
+        os.rename(dali_ini_filename, dali_ini_filename + '.old')
+        with open(dali_ini_filename, 'w') as fp:
+            config.write(fp)
+
+except Exception as e:
+    print('Verification failed.')
+    print(e)
+finally:
+    os.remove(local_dali_config)
+    os.remove(local_dali_config_sig)
+
+
+os.environ['LANGUAGE'] = orig_lang
