4de010151a325d638659299f246f052136962e48
Stefan Mertl <info@mertl-research.at>
2018-12-14 13:25:28 +0100

    Fixed the typo.

--- mss_record/lib/mss_record/core/recorder.py
+++ mss_record/lib/mss_record/core/recorder.py
@@ -291,7 +291,7 @@ class Recorder:
                 cur_filename = cur_trace.id.replace('.','_') + '_' + cur_trace.stats.starttime.isoformat().replace(':','') + '.msd'
                 cur_filepath = os.path.join(data_dir, cur_filename)
                 try:
-                    cur_trace.date = cur_trace.data.astype(np.int32)
+                    cur_trace.data = cur_trace.data.astype(np.int32)
                     cur_trace.write(cur_filepath,
                                     format = "MSEED",
                                     reclen = 512,
